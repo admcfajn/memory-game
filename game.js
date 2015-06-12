b40a0314e67157e3f58d9bfb010531429db0426f
dabzo <adam@dabzo.com>
2015-06-11 20:10:45 -0700

    plusCellVariants

--- game.js
+++ game.js
@@ -4,28 +4,47 @@
 	gameWidth = "500",
 	gameHeight = "500",
 	cellsWide = $(".cells_wide").val(),
-	cellsHigh = $(".cells_high").val();
+	cellsHigh = $(".cells_high").val(),
+	//numVariants = 5,
+	numVariants = $(".number_variants").val(),
+	totalCells = (cellsWide * cellsHigh);
 	//cellVariants = array(1,2,3,4,5,6,7,8);
 
-	/*
-		in plot:
-		add element,
-		push .class to array
-		bar = rand(0-totalLimit)
-		baz = rand(0-variantsLimit # variantsEachLimit)
-		foreach .class
-		if bar != done{
-			addClass baz
-			.class = done
-		}
-	*/
+	variantClasses = [];
+	cellList = [];
+
+function build_variants(total_variants){
+	variantList = [];
+	for(x=0;x<=total_variants;x++){
+		variantList.push([]);
+		variantList[x].push('variant_'+x);
+		variantList[x].push(total_variants);
+	}
+	return variantList;
+}
+
+function assign_variant(elem, variant_list, total_variants){
+	currentVariant = getRandomInt(0,total_variants);
+	currentVariantCount = variant_list[currentVariant][1];
+	if(currentVariantCount > 0){
+		elem.className = elem.className+" variant_"+currentVariant;
+		variant_list[currentVariant][1] = currentVariantCount--;
+	} else {
+		assign_variant(elem);
+	}
+}
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
 
 
-$(".cells_wide, .cells_high").change(function(){
+$(".cells_wide, .cells_high, .number_variants").change(function(){
 	cellsWide = $(".cells_wide").val();
 	cellsHigh = $(".cells_high").val();
+	numVariants = $(".number_variants").val();
 	clear_children(containerWrapper);
-	plot(containerWrapper,cellsWide,cellsHigh);
+	plot(containerWrapper,cellsWide,cellsHigh,numVariants);
 });
 
 function clear_children(target){
@@ -34,8 +53,10 @@ function clear_children(target){
 	}
 }
 
-function plot(container,cellswide,cellshigh){
+function plot(container, cellswide, cellshigh, total_variants){
 	
+	variants = build_variants(total_variants);
+	//alert(variants+' ya');
 	for(x=0;x<cellshigh;x++){
 		//var cols.push(array())
 		
@@ -60,20 +81,31 @@ function plot(container,cellswide,cellshigh){
 					cellContainer.style.width = (gameWidth/cellswide)+"px";
 					cellContainer.style.height = (gameHeight/cellshigh)+"px";
 					cellContainer.id = "cell_"+x+"_"+y;
-					cellContainer.className = "cell cell"+x+"_cell"+y;
+					cellContainer.className = "cell cell_"+x+"_"+y;
+
+					cellList.push([]);
+					cellCoords = (x * cellswide) + y;
+
+					cellList[cellCoords].push("cell_"+x+"_"+y);
+					cellList[cellCoords].push(total_variants);
+
+					assign_variant(cellContainer, variants, total_variants);
+					//alert(cellList);
+					//assign_variant(cellContainer);
 			}
 	}
 }
  
 
  function init(){
- 	plot(containerWrapper,cellsWide,cellsHigh);
+ 	plot(containerWrapper, cellsWide, cellsHigh, numVariants);
+ 	//build_variants(variantClasses);
  }
 
 
 /*
 
-NOTE: DO SPEED-TESTS ON SAME APP WITH SYNCHRONIZED / UN  CLASS - JS-VAR NAMES / SELECTORS?
+NOTE: Find Optimal css id/class + js var / funct nomenclature?
 homeScreen
 	startInput
 		[wide]x[tall]=[totalCards] Min 2x2
