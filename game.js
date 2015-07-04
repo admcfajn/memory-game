9d26c786a1f90115c8fd6a81834c7ccea8edb45a
dabzo <adam@dabzo.com>
2015-07-03 22:41:15 -0700

    hexValueGenerator

--- game.js
+++ game.js
@@ -206,3 +206,42 @@ gameLoop
 	whosTurn
 	cardShow
 */
+/*
+function build_colours(){
+
+var permArr = [],
+  usedChars = [];
+
+function permute(input) {
+  var i, ch;
+  for (i = 0; i < input.length; i++) {
+    ch = input.splice(i, 1)[0];
+    usedChars.push(ch);
+    if (input.length == 0) {
+      permArr.push(usedChars.slice());
+    }
+    permute(input);
+    input.splice(i, 0, ch);
+    usedChars.pop();
+  }
+  return permArr
+};
+document.write(JSON.stringify(permute(['0', '3', '6', '9', 'c', 'f'])));
+
+
+// var digits = ['0','3','6','9','c','f'];
+// var hexlist = [];
+// var l = digits.length;
+
+// for(a=0;a<l;a++){
+// 	for(b=0;b<l;b++){
+// 		hexlist.push(digits[a]);
+// 		hexlist[a*l+b]=hexlist[a*l+b]+=digits[b];
+// 		hexlist[a*l+b]=hexlist[a*l+b]+=digits[b];
+// 	}
+// }
+
+// console.log(hexlist);
+}
+
+*/
